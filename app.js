0fac2acd799191b31d44b8064d9c9edb9ccd104c
Toubic <tj22fe@student.lnu.se>
2017-06-30 01:12:19 +0200

    Hello World express

---
+++ app.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+
+try {
+
+    app.get('/', function (req, res) {
+        res.send('Hello World')
+    });
+
+    app.listen(process.env.PORT || 3000, function () {
+        console.log("Server started!");
+    });
+}
+catch (e){
+    console.log(e.message);
+}
