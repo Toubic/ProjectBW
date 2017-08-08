81caf0f6519f84946c96afc9f9ddb4955fac5c4d
Toubic <tj22fe@student.lnu.se>
2017-08-08 02:20:27 +0200

    Add markers for Sweden and divide js into several files

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,5 +1,34 @@
 'use strict';
 
+function createMarkerWithInfo(lat, long, title, link, map) {
+    var marker = new google.maps.Marker({
+        position: {lat: lat, lng: long},
+        map: map
+    });
+
+    var content =
+        '<h2>' + title + '</h2>'+
+        '<a href="' + link + '">'+
+        '' + link + '</a>';
+
+
+    var infoWindow = new google.maps.InfoWindow({
+        content: content
+    });
+    marker.addListener('click', function() {
+        infoWindow.open(map, marker);
+    });
+
+}
+
+function generateMarkersForWorldMap(map) {
+
+    //Sweden:
+    createMarkerWithInfo(59.8552777778, 17.6319444444, 'Uppsala Boogie Woogie Weekend', 'http://swingkatten.se/lagerochevenemang', map);
+    createMarkerWithInfo(60.128201, 18.648209, 'Herräng Dance Camp Week 1', 'https://www.herrang.com/program/week-1', map);
+
+}
+
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 2,
@@ -86,28 +115,7 @@ function initMap() {
         ]
     });
 
-    function createMarkerWithInfo(lat, long, title, link) {
-        var marker = new google.maps.Marker({
-            position: {lat: lat, lng: long},
-            map: map
-        });
-
-        var content =
-            '<h2>' + title + '</h2>'+
-            '<a href="' + link + '">'+
-            '' + link + '</a>';
-
-
-        var infoWindow = new google.maps.InfoWindow({
-            content: content
-        });
-        marker.addListener('click', function() {
-            infoWindow.open(map, marker);
-        });
-
-    }
-
-    createMarkerWithInfo(59.8552777778, 17.6319444444, 'Uppsala Boogie Woogie Weekend', 'http://swingkatten.se/lagerochevenemang');
+    generateMarkersForWorldMap(map);
 }
 
 initMap();
